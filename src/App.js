accff1bff5ac78798350a9585230e633cf044ecf
shao <ting010204@gmail.com>
2019-04-24 23:11:04 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
   this.onTyping       = this.onTyping.bind(this);
   this.timerStart     = this.timerStart.bind(this);
   this.timerStop      = this.timerStop.bind(this);
+  this.setArticle     = this.setArticle.bind(this);
+  this.newArticle     = this.newArticle.bind(this);
 
   this.state = {
       article   :"no article.",
@@ -17,27 +19,40 @@ class App extends Component {
       correct:true,
       input :'',
       started:false,
-      inputShow:'',
       inputNum:0,
       wpm:0,
       changedNum:0,
       timer:0,
       timerId:'',
-      page:''
+      page:0,
+      articles:[]
     };
   }
 
   fetchartical(){
-    //fetch
+    //tmp article
     const article = "Scrum is a framework within which people can address complex adaptive problems, while productively and creatively delivering products of the highest possible value.";
-    //article to arry
-    var tmparticle = article.split("");
-    this.setState({
-      article:article,
-      articleArry:tmparticle
+    //fetch
+    const url     ="https://newsapi.org/v2/top-headlines?";
+    const country ="country=us&"
+    const key     ="apiKey=1c285c377280404cbfba2dbfaa5d3bec";
+
+    fetch(`${url}${country}${key}`)
+      .then(response => response.json())
+      .then(result => this.setArticle(result,this.state.page))
+      .catch(error => error);
+
+  }
+  setArticle(res,page){
+    var tmparticle = res.articles[page].description.split("");
+     this.setState({
+      res,
+      article     :res.articles[page].description,
+      articleArry :tmparticle
     })
 
   }
+
   timerStart(){
     var timerInerval = setInterval(()=>{
       this.setState({
@@ -69,6 +84,7 @@ class App extends Component {
     var tmp = true;
 
     for(var i=0;i<inputTerm.length;i++){
+      console.log(articleArry[i],inputTerm[i])
       if(articleArry[i]==inputTerm[i]){
         tmp=true;
       }
@@ -77,6 +93,7 @@ class App extends Component {
         break;
       }
     }
+
 
     var wpm = parseInt(inputTerm.length)/parseInt(this.state.timer)*12;
     wpm = wpm.toFixed(2);
@@ -89,6 +106,24 @@ class App extends Component {
     });
   }
 
+  newArticle(){
+    this.timerStop();
+    this.setState({
+      page: this.state.page + 1,
+      correct:true,
+      input :'',
+      started:false,
+      inputNum:0,
+      wpm:0,
+      changedNum:0,
+      timer:0,
+      timerId:'',
+    })
+    document.getElementById('input').value="";
+
+    this.setArticle(this.state.res,this.state.page + 1);
+  }
+
  componentDidMount(){
     this.fetchartical();
  }
@@ -98,44 +133,35 @@ class App extends Component {
  }
 
  render() {
+  const{ articles } = this.state;
+
   return(
     <div className='container'>
-      <div className='naviarea'>
-        <nav>
-          <ul>
-            <li>
-              <a href='#'>English</a>
-            </li>
-            <li>
-              <a href='#'>Chinese</a>
-            </li>
-            <li>
-              <a href='#'>code</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
       <div className='articalarea'>
         <textarea className='typebox' readOnly='ture' value={this.state.article}/>
       </div>
 
       <div className='typearea'>
-        <textarea className={"typebox " + (this.state.correct? 'Green':'Red')} type ='text' onChange={this.onTyping} placeholder="type here to start"/>
+        <textarea id='input' className={"typebox " + (this.state.correct? 'Green':'Red')} type ='text' onChange={this.onTyping} placeholder="type here to start"/>
       </div>
 
       <div className='infoarea1'>
-        <h3>timer:{this.state.timer}</h3>
-        <h3>letter:{this.state.inputNum}</h3>
-        <h3>WPM:{this.state.wpm}</h3>
+        <div className='tocenter'>
+          <h2>WPM:{this.state.wpm}</h2>
+          <h4>time:{this.state.timer}s  letter:{this.state.inputNum}</h4>
+        </div>
       </div>
       <div className='infoarea2'>
-        Ad here
+        <div className='tocenter'><h2>maybe AD here</h2></div>
       </div>
       <div className='btnarea1' onClick={this.timerStop}>
-        timer stop
+        <div className='tocenter'><h2>timer stop</h2></div>
       </div>
-      <div className='btnarea2'>
-        change article.
+      <div className='btnarea2' onClick={this.newArticle}>
+        <div className='tocenter'>
+          <h2>article:{this.state.page+1}/20</h2>
+          <h4>Click me to change article</h4>
+        </div>
       </div>
     </div>
   );
